460868e6314a0bdd8a3befb45beb6dd0dd5e938f
Scott <scotthuston1982@gmail.com>
2018-01-04 14:29:18 -0500

    final commit

---
+++ naughty_letter_writer.rb
@@ -0,0 +1,28 @@
+require 'erb'
+require 'pry'
+
+kids_data = File.read('data/kids-data.txt')
+naughty_letter = File.read('templates/naughty_letter_template.txt.erb')
+
+kids_data.each_line do |kid|
+
+  kid_data_array = kid.split
+
+  name     = kid_data_array[0]
+  behavior = kid_data_array[1]
+  toys     = kid_data_array[2..7]
+
+
+  infraction = kid.split("|").last
+
+  next unless behavior == 'naughty'
+
+  filename    = 'letters/naughty/' + name + '.txt'
+  letter_text = ERB.new(naughty_letter, nil, '-').result(binding)
+
+  puts "Writing #{filename}."
+  File.write(filename, letter_text)
+
+end
+
+puts 'Done!'
